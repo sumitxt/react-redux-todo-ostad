e13f730eed2bd2735703da0c12bbec9470e5a64c
sumitxt <sumitbarua14@gmail.com>
2023-01-05 18:58:49 +0600

    delete and update

---
+++ src/components/todo/TodoDeleteAlert.js
@@ -0,0 +1,25 @@
+import Swal from "sweetalert2";
+import {RemoveTodo} from "../../redux/state/todo/todoSlice";
+import store from "../../redux/store/Store";
+
+export function TodoDeleteAlert(i){
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            store.dispatch(RemoveTodo(i))
+            Swal.fire(
+                'Deleted!',
+                'Your task has been deleted.',
+                'success'
+            )
+        }
+    })
+
+}
--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {useSelector} from "react-redux";
+import {TodoDeleteAlert} from "./TodoDeleteAlert";
+import {TodoEditAlert} from "./TodoEditAlert";
 
 const TodoList = () => {
 
@@ -25,8 +27,8 @@ const TodoList = () => {
                                 <tr key={i.toString()}>
                                     <td>{i}</td>
                                     <td>{item}</td>
-                                    <td><button className="btn btn-warning">Edit</button></td>
-                                    <td><button className="btn btn-danger">Remove</button></td>
+                                    <td><button onClick={()=>{TodoEditAlert(i,item)}} className="btn btn-warning">Edit</button></td>
+                                    <td><button onClick={()=>{TodoDeleteAlert(i)}} className="btn btn-danger">Remove</button></td>
                                 </tr>
                             )
                         })}
